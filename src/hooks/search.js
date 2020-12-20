811635e1e9dc759d27d1299d5a354f7ec64ae7f2
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-20 07:34:01 +0000

    Created search hook

---
+++ src/hooks/search.js
@@ -0,0 +1,34 @@
+import { watch, ref, computed } from 'vue';
+
+const useSearch = (items, searchProp) => {
+  const enteredSearchTerm = ref('');
+  const activeSearchTerm = ref('');
+
+  function updateSearch(val) {
+    enteredSearchTerm.value = val;
+  }
+
+  // Computes
+  const availableItems = computed(() => {
+    let filteredItems = [];
+    if (activeSearchTerm.value) {
+      filteredItems = items.filter((item) =>
+        item[searchProp].includes(activeSearchTerm.value)
+      );
+    } else if (items) {
+      filteredItems = items;
+    }
+    return filteredItems;
+  });
+
+  watch(enteredSearchTerm, (val) => {
+    setTimeout(() => {
+      if (val === enteredSearchTerm.value) {
+        activeSearchTerm.value = val;
+      }
+    }, 300);
+  });
+  return { enteredSearchTerm, updateSearch, availableItems };
+};
+
+export default useSearch;
